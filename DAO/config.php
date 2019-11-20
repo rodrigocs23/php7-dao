35a82d2c2a222d47f446e837fda5d31ea127144e
Rodrigo Carvalho Silva <csr.iron@gmail.com>
2019-11-20 19:36:41 -0400

    Include DIRECTOR_SEPARATOR

--- DAO/config.php
+++ DAO/config.php
@@ -3,8 +3,8 @@
 spl_autoload_register(function($class_name){
     $file_name = $class_name .".php";
 
-    if (file_exists("class/".$file_name)) {
-        require_once("class/".$file_name);
+    if (file_exists("class".DIRECTORY_SEPARATOR.$file_name)) {
+        require_once("class".DIRECTORY_SEPARATOR.$file_name);
     }
 });
 ?>
